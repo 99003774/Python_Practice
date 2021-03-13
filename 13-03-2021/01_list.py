ccfe52576edfa20a9a6d05382339e48cb7e75fdb
99003774 <kumar.utsav@ltts.com>
2021-03-13 10:45:58 +0530

    List examples by Linux using VS Code.

---
+++ 13-03-2021/01_list.py
@@ -0,0 +1,12 @@
+# List declaration of same type
+
+places = ['Jaipur','Delhi','Gurgaon','Panaji']
+print(places)
+
+# list declaration of different types
+
+people = [1, 'Jai', 2, 'Bubna']
+print (people)
+
+print(places == people)  # compare one list with other
+
