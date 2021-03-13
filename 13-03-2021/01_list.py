7a09d329ccd5ad1becc8d56989547217e2da46a4
99003774 <kumar.utsav@ltts.com>
2021-03-13 12:08:08 +0530

    List

--- 13-03-2021/01_list.py
+++ 13-03-2021/01_list.py
@@ -19,5 +19,22 @@ print(nums[0:])
 
 # get the data by indexing in list
 List =[1,2,23,35,49,55]
-print(List[:3])
+print(List[:3])    # It will print till 3rd number
+print(List[-3:])   # It will print from back side
 
+# Iterating a List
+
+names = ["John","Sangam","Danish","Helen","Doremon"]
+for i in names:   # The i variable will iterate over the elements of the lISTS
+    print (i)
+
+#=============================
+#Declaring the empty list
+
+li =[]
+n = int(input("Enter the number of elements: \n"))  #Number of elements are entered by the user
+for i in range(0,n):
+    li.append(input("Enter the data\n"))
+print("The datas are:\n")
+for i in li:
+    print(i,end =", ")
