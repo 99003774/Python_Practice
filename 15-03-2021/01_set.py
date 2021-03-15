8932aa68ec609d218649c9f13fb1361380e298aa
99003774 <kumar.utsav@ltts.com>
2021-03-15 09:56:50 +0530

    01_set by Linux OS using VS Code

---
+++ 15-03-2021/01_set.py
@@ -0,0 +1,10 @@
+# set example
+
+places = {"Goa", "Bangalore", "Mysore", "Pune", "Delhi", "Gurgaon"}
+#print(places)
+places.discard("Bangalore");    # Removing one place from places
+print(places)
+
+
+for i in places:
+    print(i)
