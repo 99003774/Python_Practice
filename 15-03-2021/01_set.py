90cae4f016e1edb8f9de24dee4e1e0b3fdf9dae2
99003774 <kumar.utsav@ltts.com>
2021-03-15 10:18:20 +0530

    Updated code

--- 15-03-2021/01_set.py
+++ 15-03-2021/01_set.py
@@ -3,8 +3,11 @@
 places = {"Goa", "Bangalore", "Mysore", "Pune", "Delhi", "Gurgaon"}
 #print(places)
 places.discard("Bangalore");    # Removing one place from places
-print(places)
-
 
 for i in places:
     print(i)
+
+# Using Update function
+
+places.update(["Patna"])    
+print(places)
